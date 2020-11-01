86c907ba014bc45e257efc94a47d34917f698a73
davisantosr <davi-rocha2@hotmail.com>
2020-10-31 22:30:03 -0300

    added page of details of the contact|handle error 404

--- contatos/views.py
+++ contatos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 from .models import Contato
 
 def index(request):
@@ -6,3 +7,13 @@ def index(request):
     return render(request, 'contatos/index.html', {
         'contatos': contatos
     })
+
+def ver_contato(request, contato_id):
+    try:
+        contato = Contato.objects.get(id=contato_id)
+        return render(request, 'contatos/ver_contato.html', {
+            'contato': contato
+        })
+    except Contato.DoesNotExist as e:
+        raise Http404()
+
