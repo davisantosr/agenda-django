369ee25a9353f0fe089706f433e19b6283e56586
davisantosr <davi-rocha2@hotmail.com>
2020-11-01 14:28:52 -0300

    image field|config settings and urls to work with images

--- contatos/models.py
+++ contatos/models.py
@@ -15,7 +15,7 @@ class Contato(models.Model):
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
     mostrar = models.BooleanField(default=True)
-
+    fotos = models.ImageField(blank=True, upload_to='fotos/%Y/%m/')
     def __str__(self):
         return self.nome
 
