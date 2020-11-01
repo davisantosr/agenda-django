8e8926aa67d3460c047c528acdf16931b5cc2782
davisantosr <davi-rocha2@hotmail.com>
2020-10-31 23:53:22 -0300

    added show/hide to models|pagination|fixed user details when hide is check

--- contatos/views.py
+++ contatos/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Contato
+from django.core.paginator import Paginator
 
 def index(request):
     contatos = Contato.objects.all()
+    paginator = Paginator(contatos, 4)
+
+    page_number = request.GET.get('p')
+    contatos = paginator.get_page(page_number)
+
     return render(request, 'contatos/index.html', {
         'contatos': contatos
     })
 
 def ver_contato(request, contato_id):
 #  contato = Contato.objects.get(id=contato_id)
-   contato = get_object_or_404(Contato, id=contato_id)
-   return render(request, 'contatos/ver_contato.html', {
+    contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
+
+    return render(request, 'contatos/ver_contato.html', {
         'contato': contato
     })
 
-    
