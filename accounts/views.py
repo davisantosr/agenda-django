c6d69738625aca78ce3844215d2677e8d730090e
davisantosr <davi-rocha2@hotmail.com>
2020-11-01 21:12:37 -0300

    login logout dashboard|verifying user logged in

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,35 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.core.validators import validate_email
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 def login(request):
-    return render(request, 'accounts/login.html')
+
+    if request.method != 'POST':
+        return render(request, 'accounts/login.html')
+
+    usuario = request.POST.get('usuario')
+    senha = request.POST.get('senha')
+
+    user = auth.authenticate(request, username=usuario, password=senha)
+
+    if not user:
+        messages.error(request, 'Usuário ou senha inválidos')
+        return render(request, 'accounts/login.html')
+    else:
+        auth.login(request, user)
+        return redirect('dashboard')
+        messages.success(request, 'Login com sucesso')
 
 def logout(request):
-    return render(request, 'accounts/logout.html')
+    auth.logout(request)
+    return redirect('index')
 
 def cadastro(request):
     if request.method != 'POST':
         return render(request, 'accounts/cadastro.html')
+
     nome = request.POST.get('nome')
     sobrenome = request.POST.get('sobrenome')
     email = request.POST.get('email')
@@ -50,11 +68,12 @@ def cadastro(request):
         messages.error(request, 'Email já existe')
         return render(request, 'accounts/cadastro.html')
 
-    messages.success(request, 'Registrado com sucesso. Faça o login.')
     user = User.objects.create_user(username=usuario, email=email, password=senha,
                                     first_name=nome, last_name=sobrenome)
     user.save()
+    messages.success(request, 'Registrado com sucesso. Faça o login.')
     return redirect('login')
 
+@login_required(redirect_field_name='login')
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
